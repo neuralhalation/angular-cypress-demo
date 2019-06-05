e2aa4dbd493c7b9169ab2296e9bd7f57a60c7057
Lyon Lenk <neuralhalation@gmail.com>
2019-06-05 14:55:31 -0500

    pattern.js added

--- cypress/integration/patterns.js
+++ cypress/integration/patterns.js
@@ -5,6 +5,18 @@ describe('/some_endpoint_w_iframe', () => {
     });
 
     it('has an element class that all produce the same behavior w/ click', () => {
-        cy.get
+        cy.get('#iframe').iframe().find('.some-class').each(($el) => {
+            cy.wrap($el).then(($element) => {
+                const elementState = cy.get($element).then(() => {
+                    return $element.css('changing-property');
+                });
+
+                cy.get($element).click();
+
+                cy.get($element).should(() => {
+                    expect($element.css('changing-property')).not.to.eq(elementState);
+                });
+            });
+        });
     });
 });
